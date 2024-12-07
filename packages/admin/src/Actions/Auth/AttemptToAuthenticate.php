d809ac04895963c332328ed63707a5785d9f612f
Arthur Monney <monneylobe@gmail.com>
2024-12-07 02:37:54 +0100

    Add zone discount (#311)
    * chore: move authentication action to an Auth actions folder
    
    * feat: upgrade dependencies and remove github actions for php8.2 and laravel 10
    
    * style: rebase 2.x

---
+++ packages/admin/src/Actions/Auth/AttemptToAuthenticate.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopper\Actions\Auth;
+
+use Closure;
+use Illuminate\Validation\ValidationException;
+use Shopper\Facades\Shopper;
+
+class AttemptToAuthenticate
+{
+    public function handle(array $request, Closure $next)
+    {
+        $isLoggedIn = Shopper::auth()->attempt([
+            'email' => $request['email'],
+            'password' => $request['password'],
+        ], $request['remember']);
+
+        if (! $isLoggedIn) {
+            $this->throwFailedAuthenticationException();
+        }
+
+        return $next($isLoggedIn);
+    }
+
+    protected function throwFailedAuthenticationException(): void
+    {
+        throw ValidationException::withMessages([
+            'email' => __('shopper::pages/auth.login.failed'),
+        ]);
+    }
+}
---
+++ packages/admin/src/Actions/Auth/EnableTwoFactorAuthentication.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shopper\Actions\Auth;
+
+use Illuminate\Support\Collection;
+use Shopper\Contracts\TwoFactorAuthenticationProvider;
+use Shopper\Events\TwoFactor\TwoFactorAuthenticationEnabled;
+
+class EnableTwoFactorAuthentication
+{
+    public function __construct(protected TwoFactorAuthenticationProvider $provider) {}
+
+    public function __invoke($user): void
+    {
+        $user->forceFill([
+            'two_factor_secret' => encrypt($this->provider->generateSecretKey()),
+            'two_factor_recovery_codes' => encrypt(json_encode(
+                Collection::times(8, fn () => RecoveryCode::generate())->all()
+            )),
+        ])->save();
+
+        TwoFactorAuthenticationEnabled::dispatch($user);
+    }
+}
